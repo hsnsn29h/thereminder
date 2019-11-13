64a87f6d0f7a5eb8ee4e8a37951dd55f077148fb
hsnsn29h <48161684+hsnsn29h@users.noreply.github.com>
2019-11-13 19:50:09 +0300

    the begining e3

--- app/src/main/java/com/example/thereminder/MainActivity.java
+++ app/src/main/java/com/example/thereminder/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.thereminder;
 import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,23 +21,51 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
         SharedPreferences prefs=getSharedPreferences("prefs",MODE_PRIVATE);
+
         boolean firstStart=prefs.getBoolean("firstStart",true);
 
-        if(true){ firstStartMethod();}
+
+        if(firstStart){ firstStartMethod();}
 
     }
 
     private void firstStartMethod() {
-        Dialog firstD=new Dialog(this);
+        final SharedPreferences prefs=getSharedPreferences("prefs",MODE_PRIVATE);
+        final SharedPreferences.Editor editor=prefs.edit();
+        final Dialog firstD=new Dialog(this);
         firstD.setContentView(R.layout.first_start_dialog);
 firstD.show();
+        Button finishButton=firstD.findViewById(R.id.finishB);
+
+        final CheckBox mercyCB=firstD.findViewById(R.id.mercy);
+        final CheckBox respectCB=firstD.findViewById(R.id.respect);
+        final CheckBox ropCB=firstD.findViewById(R.id.ROP);
+        final CheckBox honestyCB=firstD.findViewById(R.id.honesty);
+        final CheckBox trustCB=firstD.findViewById(R.id.trust);
+        final CheckBox justiceCB=firstD.findViewById(R.id.justice);
+
+
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editor.putBoolean("قيمة الرحمة",mercyCB.isChecked());
+                editor.putBoolean("قيمة الاحترام",respectCB.isChecked());
+                editor.putBoolean("قيمة بر الوالدين",ropCB.isChecked());
+                editor.putBoolean("قيمة الصدق",honestyCB.isChecked());
+                editor.putBoolean("قيمة الأمانة",trustCB.isChecked());
+                editor.putBoolean("قيمة العدل",justiceCB.isChecked());
+                firstD.dismiss();
+
+            }
+        });
 
 
 
 
-SharedPreferences prefs=getSharedPreferences("prefs",MODE_PRIVATE);
-        SharedPreferences.Editor editor=prefs.edit();
+
         editor.putBoolean("firstStart",false);
         editor.apply();
     }
